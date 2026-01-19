02a4de29614ad9ef7a5a0b8c06829b45c3434635
dru <diablo_lecornu@hotmail.fr>
2026-01-19 15:12:12 +0100

    initial

---
+++ syntaxe.py
@@ -0,0 +1,22 @@
+'''
+
+Docstring for Class01_Cellule
+'''
+#------------------------------
+class Cellule: #declaration d'une classe objet, mot clef 'class'
+    "Modelisation de la cellule machine"
+    mode = ("Arret", "Manuel", "Auto") #attribut
+    mode_courant = 0 #attribut
+
+    def Marche(self): #methode attachee par le mot clef 'self'
+        "Demarrage de la cellule"
+        self.mode_courant = 1 #procedure appelant les attributs via self
+
+    def GetMode(self): #methode
+        "Retourne le mode de la cellule"
+        return self.mode[self.mode_courant] #retour de la fonction
+    
+    def Arret(self): #methode
+        "Arret de la cellule"
+        pass #traitement de la fonction non encore definit
+    
