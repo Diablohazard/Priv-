783331b8a82d01a3b57955a7a6ec1b1a2da43c49
dru <diablo_lecornu@hotmail.fr>
2026-01-19 17:18:36 +0100

    ajout condition

--- EXO_1.py
+++ EXO_1.py
@@ -4,3 +4,7 @@ class PositionTool: #declaration du tableau des positions
     Home = [10.0, 10.0, 500.0] #attribut des coordonnées Home
     Pick = [100.0, 30.0, 120.0] #attribut des coordonnées Pick
     Place = [100.0, 150.0, 230.0] #attribut des coordonnées Place
+
+def GetStatus(): #methode
+    "Retourne le status de la position"
+    return f"Position Tool : {str(PositionTool.position)}\n"
