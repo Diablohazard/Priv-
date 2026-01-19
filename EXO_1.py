42d99883aa3a53f9ebf50b6bf362045b3b62dd34
dru <diablo_lecornu@hotmail.fr>
2026-01-19 17:08:54 +0100

    class positiontool

---
+++ EXO_1.py
@@ -0,0 +1,6 @@
+class PositionTool: #declaration du tableau des positions
+    position = ["X", "Y", "Z"] #attribut des coordonnées
+    init = [0.0, 0.0, 0.0] #attribut des coordonnées initiales
+    Home = [10.0, 10.0, 500.0] #attribut des coordonnées Home
+    Pick = [100.0, 30.0, 120.0] #attribut des coordonnées Pick
+    Place = [100.0, 150.0, 230.0] #attribut des coordonnées Place
