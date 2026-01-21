f4eaa67f41da776509875e7e2ad4a530ca45e57e
dru <diablo_lecornu@hotmail.fr>
2026-01-21 08:41:44 +0100

    restructuration

--- EXO_1.py
+++ EXO_1.py
@@ -17,20 +17,10 @@ class Robot:
     }
 
     # Position courante outil
-    pos_Tool = POSITIONS["Home"]
+    pos_Tool = POSITIONS["init"]
 
-    # ========= Méthodes =========
-    def GetStatus(self):
-        x, y, z = self.pos_Tool
-        print("marque\tstate_OK\tnb_alarme\tX\tY\tZ")
-        print(f"{Robot.marque}\t{Robot.state_OK}\t{Robot.nb_alarme}\t\t{x}\t{y}\t{z}")
-        print(
-            f"{Robot.marque} Status "
-            f"{'OK' if Robot.state_OK else 'DEFAULT'} "
-            f"({Robot.nb_alarme}) "
-            f"Position X={x} Y={y} Z={z}"
-        )
-        print("-" * 60)
+    def Moveinit(self):
+        self.pos_Tool = Robot.POSITIONS["init"]
 
     def MoveHome(self):
         self.pos_Tool = Robot.POSITIONS["Home"]
@@ -48,6 +38,19 @@ class Robot:
     def ClearDefault(self):
         Robot.state_OK = True
 
+        # ========= Méthodes =========
+    def GetStatus(self):
+        x, y, z = self.pos_Tool
+        print("marque\tstate_OK\tnb_alarme\tX\tY\tZ")
+        print(f"{Robot.marque}\t{Robot.state_OK}\t{Robot.nb_alarme}\t\t{x}\t{y}\t{z}")
+        print(
+            f"{Robot.marque} Status "
+            f"{'OK' if Robot.state_OK else 'DEFAULT'} "
+            f"({Robot.nb_alarme}) "
+            f"Position X={x} Y={y} Z={z}"
+        )
+        print("-" * 60)
+
 
 # =====================================================
 # Jeu de test (conforme à l’énoncé)
@@ -55,18 +58,13 @@ class Robot:
 
 print(Robot.marque)  # utilisation attribut de classe sans instanciation
 
-rob1 = Robot()  # première instance
-rob2 = Robot()  # seconde instance
+rob = Robot()  # première instance
 
-# Ordres sur robot 1
-rob1.GetStatus()
-rob1.MoveHome()
-rob1.RaiseDefault()
-rob1.GetStatus()
-rob1.ClearDefault()
-rob1.GetStatus()
-
-# Ordres sur robot 2
-rob2.GetStatus()
-rob2.RaiseDefault()
-rob2.GetStatus()
+# Ordres sur robot
+rob.Moveinit()
+rob.GetStatus()
+rob.MoveHome()
+rob.RaiseDefault()
+rob.GetStatus()
+rob.ClearDefault()
+rob.GetStatus()
