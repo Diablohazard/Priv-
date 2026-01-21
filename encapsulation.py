1e2c7cedef28bc55b30859dc56ceecbd772fa8af
dru <diablo_lecornu@hotmail.fr>
2026-01-21 15:18:53 +0100

    fonctionnel mais valeurs ne changent pas

--- encapsulation.py
+++ encapsulation.py
@@ -9,20 +9,20 @@ class Produit:
     }
 
     # ========= Attributs d'instance =========
-    def __init__(self, id=1):
-        self.material = "Bois"
-        self.length_m = 0.5
-        self.width_m = 0.5
-        self.height_m = 0.5
-        self.volume_m3 = 0.125
-        self.mass_kg = 87.5
+    def __init__(self, material=str, length_mm=int, width_mm=int, height_mm=int):
+        self.material = material
+        self.length_m = length_mm / 1000
+        self.width_m = width_mm / 1000
+        self.height_m = height_mm / 1000
+        self.volume_m3 = self.length_m * self.width_m * self.height_m
+        self.mass_kg = self.volume_m3 * self.DENSITY[self.material]
 
     # ========= Méthodes =========
     def ComputeVolume(self):
-        self.volume_m3 = self.length_m * self.width_m * self.height_m
+        return self.volume_m3
 
     def ComputeMass(self):
-        self.mass_kg = self.volume_m3 * self.DENSITY[self.material]
+        return self.mass_kg
 
     def __str__(self):
         return f"material={self.material} longueur=({self.length_m}, largeur={self.width_m}, hauteur={self.height_m}) volume={self.volume_m3}m3 mass={self.mass_kg}kg"
@@ -34,7 +34,7 @@ class Produit:
 
 if __name__ == "__main__":#structure conditionnelle de test
 
-    p1 = Produit("bois", 500, 500, 500)  # première instance
+    p1 = Produit("Bois", 500, 500, 500)  # première instance
 
     print(p1)
 
